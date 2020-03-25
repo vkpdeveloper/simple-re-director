9e046eda6ef2b50a6dd9199001195186624d35c6
Vaibhav Pathak <vaibhavpathak9984@gmail.com>
2020-03-25 23:10:06 +0530

    version 2.0

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var urlParams;
-var mainDiv = document.getElementById('main');
+let mainDiv = document.getElementById('main');
 (window.onpopstate = function () {
     var match,
         pl = /\+/g,
@@ -11,21 +11,25 @@ var mainDiv = document.getElementById('main');
     while (match = search.exec(query))
         urlParams[decode(match[1])] = decode(match[2]);
 })();
-if(!Object.keys(urlParams).length == 0){
+if (!Object.keys(urlParams).length == 0) {
     if (urlParams['redirectTo'].includes('http://') || urlParams['redirectTo'].includes('https://')) {
-    console.log(`Process Started at ${urlParams['redirectTo']}`);
-    document.getElementsByTagName('title')[0].text = "redirecting...";
-    let data = document.createTextNode(`Redirecting to ${urlParams['redirectTo']}`)
-    mainDiv.appendChild(data);
-    setTimeout(() => {
-        window.location.replace(urlParams['redirectTo'])
-    }, 10000);
-}else{
-    document.getElementsByTagName('title')[0].text = "Invalid URL !";
-    let data = document.createTextNode(`Given URL is not a valid URL. URL must be look like : https://example.com or http://www.example.com `)
-    mainDiv.appendChild(data);
-}
-}else{
+        document.getElementsByTagName('title')[0].text = "redirecting...";
+        let data;
+        let timeOut = 10;
+        setInterval(() => {
+            mainDiv.innerText = `Redirecting to ${urlParams['redirectTo']} in ${timeOut} seconds.`;
+            if(timeOut == 0){
+                window.location.replace(urlParams['redirectTo']);
+            }else{
+                timeOut -= 1;
+            }
+        }, 1000);
+    } else {
+        document.getElementsByTagName('title')[0].text = "Invalid URL !";
+        let data = document.createTextNode(`Given URL is not a valid URL. URL must be look like : https://example.com or http://www.example.com `)
+        mainDiv.appendChild(data);
+    }
+} else {
     let data = document.createTextNode(`Welcome to re-director built by Vaibhav Pathak`)
     mainDiv.appendChild(data);
 }
